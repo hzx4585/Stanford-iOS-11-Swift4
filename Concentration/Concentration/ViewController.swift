d8b07ddbd53cdf8da40a80eca7b1680b8a9360c4
hzx4585 <519907066@qq.com>
2018-09-12 16:28:48 +0800

    加入不同表情

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -16,8 +16,6 @@ class ViewController: UIViewController {
             flipCountLabel.text = "Flips: \(flipCount)"
         }     }
     
-    var emojiChoices = ["🎃","🐢","🎃","🐢"]
-    
     @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
@@ -47,8 +45,15 @@ class ViewController: UIViewController {
         }
     }
     
+    var emojiChoices = ["🎃","🐢","🚀","⚽️","💃"]
+    var emoji = [Int:String]()
+    
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil , emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
     }
     
 }
